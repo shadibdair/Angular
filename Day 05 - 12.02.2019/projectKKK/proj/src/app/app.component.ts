ff269970b40a2dce863a2b4ccc0b5189ce42c3f9
shadibdair <43513994+shadibdair@users.noreply.github.com>
2019-04-24 08:20:28 -0400

    by shadi *ngIf="..."

---
+++ Day 05 - 12.02.2019/projectKKK/proj/src/app/app.component.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  public bobIsMale:boolean=true;
+
+  public dayOfWeek:number=new Date().getDay()+1;
+
+  public student:any;
+
+  public initStudentInfo(): void{
+    this.student={
+      "age":21,
+      "name":"Shadi"
+    }
+  }
+}
